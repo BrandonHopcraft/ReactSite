f5085099d820e3af982b192f9e3b5dd3df320a16
BrandonHopcraft <59028011+BrandonHopcraft@users.noreply.github.com>
2020-12-14 03:50:46 -0800

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          uwu
+          insert some text herrrrrrrrrrrrrrrrrrrrr
         </p>
         <a
           className="App-link"
